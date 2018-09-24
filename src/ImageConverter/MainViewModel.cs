ac67ee812e7ca243ad7dc9bc05a78720b15b1a14
Ranjith Venkatesh <Ranjith.Venkatesh@mossandlichens.com>
2018-09-24 22:23:34 +0200

    Updated to latest NuGet packages

--- src/ImageConverter/MainViewModel.cs
+++ src/ImageConverter/MainViewModel.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.ComponentModel;
-
-namespace ImgConverter
+﻿namespace ImgConverter
 {
+    using System.ComponentModel;
+
     public class MainViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void NotifyPropertyChanged(string propertyName)
         {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
     }
